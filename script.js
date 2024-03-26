4a13fa9b600da936e7a8cb6a005fb667a0bf1f94
GeorgeM11 <97505972+GeorgeM11@users.noreply.github.com>
2024-03-26 21:17:10 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,4 @@
-$(document).ready(function () {
-    // Ascunde imaginile cu clasa hidden la început
-    $('.image.hidden').hide();
 
-    function toggleImages(button) {
-        var galleryId = $(button).data('gallery');
-        var gallery = $('#' + galleryId);
-
-        // Selectăm toate imaginile din galerie
-        var allImages = gallery.find('.image');
-
-        // Verificăm dacă galeria este deja deschisă
-        var isOpen = gallery.hasClass('open');
-
-        // Verificăm dacă galeria este deschisă și conține imagini cu clasa hidden
-        if (!isOpen) {
-            // Deschide galeria și afișează toate imaginile ascunse
-            gallery.addClass('open');
-            allImages.filter('.hidden').slideDown();
-        } else {
-            // Închide galeria și ascunde imaginile care au clasa hidden
-            gallery.removeClass('open');
-            allImages.filter('.hidden').slideUp();
-        }
-    }
-
-    // Adaugă evenimentul de click pe butonul "Vezi mai multe"
-    $('.show-more-btn').on('click', function () {
-        toggleImages(this);
-    });
-});
 
 // Asigură-te că script-urile jQuery și Slick Carousel sunt încărcate înainte de acest script
 
